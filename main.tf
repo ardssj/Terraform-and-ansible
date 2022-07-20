063bc3bc9a3779bb32cebaaf64786a7723dd11b5
Andre Sousa <andreviip36@gmail.com>
2022-07-20 18:24:17 -0300

    Primeiro commit da V1

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d4ac5b634553e16"
+  instance_type = "t2.micro"
+  key_name = "iac-terraform"
+
+  tags = {
+    Name = "V2 - TerraForm Ansible Python"
+  }
+}
